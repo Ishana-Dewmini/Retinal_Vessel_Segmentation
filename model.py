a4f1bbe432999f6a1d45770b108fbb0899df70fe
Ishana-Dewmini <ishanadewmini@gmail.com>
2023-10-08 10:54:28 +0530

    update model

--- model.py
+++ model.py
@@ -6,9 +6,17 @@ from tensorflow import keras
 from keras.losses import binary_crossentropy
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+
+# Define model parameters and compile the model
+input_shape = (512, 512, 3) # (height, width, channels)
+batch_size = 16  # Number of images used in each optimization step
+num_epochs = 25 # Number of times the entire training dataset is used to learn the weights of the network
+learning_rate = 0.0001  # How fast the model learns (model parameters are updated)
 
 # Define U-Net model
-def unet_model(input_shape):
+'''def unet_model(input_shape):
     inputs = tf.keras.layers.Input(input_shape) # (512, 512, 3): 512x512 RGB images
 
     # Encoder
@@ -21,7 +29,64 @@ def unet_model(input_shape):
     output = tf.keras.layers.Conv2D(1, 1, activation='sigmoid')(concat1)    # (512, 512, 1): 1x1 convolution
 
     model = tf.keras.models.Model(inputs, output)   # Create the model
+    return model'''
+
+def unet_model(input_shape):
+    inputs = tf.keras.layers.Input(input_shape)
+    #s = tf.keras.layers.Lambda(lambda x: x / 255)(inputs)
+    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+    c1 = tf.keras.layers.Dropout(0.1)(c1)
+    c1 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = tf.keras.layers.Dropout(0.1)(c2)
+    c2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = tf.keras.layers.Dropout(0.2)(c3)
+    c3 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = tf.keras.layers.MaxPooling2D((2, 2))(c3)
+
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = tf.keras.layers.Dropout(0.2)(c4)
+    c4 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = tf.keras.layers.Dropout(0.3)(c5)
+    c5 = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    u6 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = tf.keras.layers.concatenate([u6, c4])
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = tf.keras.layers.Dropout(0.2)(c6)
+    c6 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+
+    u7 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = tf.keras.layers.concatenate([u7, c3])
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = tf.keras.layers.Dropout(0.2)(c7)
+    c7 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+
+    u8 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = tf.keras.layers.concatenate([u8, c2])
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = tf.keras.layers.Dropout(0.1)(c8)
+    c8 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+
+    u9 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = tf.keras.layers.concatenate([u9, c1], axis=3)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = tf.keras.layers.Dropout(0.1)(c9)
+    c9 = tf.keras.layers.Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = tf.keras.models.Model(inputs, outputs)   # Create the model
     return model
+
 
 # Load and preprocess your dataset (images and masks)
 def load_and_preprocess_data(data_dir):
@@ -46,19 +111,21 @@ def load_and_preprocess_data(data_dir):
     return images, masks
 
 # Split the dataset into training and validation sets
-def split_data(images, masks, validation_split=0.1, random_state=42):
+def split_data(images, masks, validation_split=0.2, random_state=42): # 80% training, 20% validation
     return train_test_split(images, masks, test_size=validation_split, random_state=random_state)
 
-# Define model parameters and compile the model
-input_shape = (512, 512, 3) # (height, width, channels)
-batch_size = 8  # Number of images used in each optimization step
-num_epochs = 50 # Number of times the entire training dataset is used to learn the weights of the network
-learning_rate = 0.0001  # How fast the model learns (model parameters are updated)
 
-model = unet_model(input_shape) # Build the model
-print(model.summary())  # Print model summary
-model.compile(optimizer=Adam(learning_rate), loss=binary_crossentropy, metrics=['accuracy'])    # Compile the model
+
+Model = unet_model(input_shape) # Build the model
+
+Model.compile(optimizer=Adam(learning_rate), loss=binary_crossentropy, metrics=['accuracy'])    # Compile the model
+print(Model.summary())  # Print model summary
 print('Model compiled successfully!')
+ 
+# Modelcheckpoint callback
+checkpointer = tf.keras.callbacks.ModelCheckpoint('model_for_vessel.h5', verbose=1, save_best_only=True)
+callbacks = [tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+                tf.keras.callbacks.TensorBoard(log_dir='logs')]
 
 # Load and preprocess your dataset
 data_dir = 'E:\\acadamic\\sem 5\\EN3160_Image Processing and Machine Vision\\project\\Project\\Data\\training'
@@ -71,11 +138,43 @@ print(f'Train images shape: {train_images.shape}, Train masks shape: {train_mask
 print(f'Validation images shape: {val_images.shape}, Validation masks shape: {val_masks.shape}')
 
 # Train the model
-model.fit(train_images, train_masks, validation_data=(val_images, val_masks), epochs=num_epochs, batch_size=batch_size)
+history = Model.fit(train_images, train_masks, validation_data=(val_images, val_masks), epochs=num_epochs, batch_size=batch_size, callbacks=callbacks)
 print('Model trained successfully!')
 
-# Save the trained model to a file
-model.save('unet_model.h5')
-print('Model saved successfully!')
+# save the model
+Model.save('unet_model2.h5')
+print ('Model saved successfully!')
+
+
+
+# Compute additional validation metrics: precision, recall, and F1-score
+val_predictions = Model.predict(val_images)
+# Convert validation masks to binary
+threshold = 0.8
+val_masks_binary = (val_masks > threshold).astype(np.uint8)
+
+# Convert validation predictions to binary
+val_predictions_binary = (val_predictions > threshold).astype(np.uint8)
+
+# Compute additional validation metrics: precision, recall, and F1-score
+precision = precision_score(val_masks_binary.flatten(), val_predictions_binary.flatten())
+recall = recall_score(val_masks_binary.flatten(), val_predictions_binary.flatten())
+f1 = f1_score(val_masks_binary.flatten(), val_predictions_binary.flatten())
+
+# Get validation accuracy using Model.evaluate()
+_, accuracy = Model.evaluate(val_images, val_masks)
+
+# Save the model summary, accuracy, precision, recall, and F1-score to a .txt file
+with open('model_summary.txt', 'w') as summary_file:
+    Model.summary(print_fn=lambda x: summary_file.write(x + '\n'))
+
+with open('validation_metrics.txt', 'w') as metrics_file:
+    metrics_file.write(f'Validation Accuracy: {accuracy}\n')
+    metrics_file.write(f'Validation Precision: {precision}\n')
+    metrics_file.write(f'Validation Recall: {recall}\n')
+    metrics_file.write(f'Validation F1-Score: {f1}\n')
+
+
+
 
 
